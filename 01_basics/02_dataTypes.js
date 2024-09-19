cf58afba039a7889e2cf482cd533fd6a819dfdb1
Aditya Sinha <141224241+adityasinha-15@users.noreply.github.com>
2024-09-19 15:23:15 +0000

    Conversion operation

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let name = "adi"
+let age = 16
+let state;
+
+//null => standalone value
+console.log(typeof null);
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,5 @@
+let isLoggedIn = "Hitesh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
