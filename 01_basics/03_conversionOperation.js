f498730c55ba5edab86126639070fad77cb8c4c1
Aditya Sinha <141224241+adityasinha-15@users.noreply.github.com>
2024-10-15 12:18:23 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,3 +3,21 @@ let isLoggedIn = "Hitesh";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(booleanIsLoggedIn);
+
+
+//Operations
+
+let value= 3;
+let negValue = -value;
+console.log(negValue);
+console.log(2**3);
+
+let str1= "hello"
+let str2 = " hi"
+let str3 = str1+str2;
+console.log(str3);
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
